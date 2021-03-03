33295ce5cab6565218f867c0872d3f78b1261324
Krishna Chaitanya Madiraju <mkrishnachaitanya@users.noreply.github.com>
2021-03-03 11:38:13 +1100

    Adding new Test Case.

---
+++ RestAssuredAPITesting_BDD_Project/src/test/java/restassuredtests/Sample_Test_To_Test_Git_Operations.java
@@ -0,0 +1,19 @@
+//Source: https://www.youtube.com/watch?v=BWD8woHxCVM&list=PLUDwpEzHYYLuMRzT6LFq4r8DwKZdcqHmY&index=5
+package restassuredtests;
+
+public class Sample_Test_To_Test_Git_Operations {
+	
+public static void main(String[] args) {
+		
+		sampleOutput();
+	}
+	
+	
+	private static void sampleOutput() {
+		 
+		System.out.println("Testing Git Operations...");
+
+	}
+	
+	
+ }
